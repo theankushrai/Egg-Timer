ec6bb020fec17e35f182eec2438a92c4df9a2491
theankushrai <iamankushrai@gmail.com>
2020-12-12 16:21:04 +0530

    1. handle foreground notifications

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,7 +1,10 @@
 package com.example.android.eggtimernotifications
 
+import android.app.NotificationManager
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -13,6 +16,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         remoteMessage?.data?.let {
             Log.d(TAG, "MessageDataPayload: ${remoteMessage.data}")
         }
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message notification Body : ${it.body}")
+            sendNotification(it.body!!)
+        }
+    }
+
+    private fun sendNotification(messagebody: String) {
+        val notificationManager=ContextCompat.getSystemService(
+            applicationContext,NotificationManager::class.java
+        )as NotificationManager
+        notificationManager.sendNotification(messagebody,applicationContext)
     }
 
     override fun onNewToken(token: String) {
