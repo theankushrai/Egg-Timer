537d6c1eb3ca4612a7d67f9086df1bcb60fa54b4
theankushrai <iamankushrai@gmail.com>
2020-12-12 16:13:26 +0530

    1. handled data notifications from firebase

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -9,6 +9,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "From : ${remoteMessage.from}")
+        // when mesage has data payload
+        remoteMessage?.data?.let {
+            Log.d(TAG, "MessageDataPayload: ${remoteMessage.data}")
+        }
     }
 
     override fun onNewToken(token: String) {
